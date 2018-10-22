b154d903039cc7993ea7436e58d73d63f8eb6b5f
Asheepun <pussagussa@gmail.com>
2018-10-22 09:32:26 +0200

    updated script

--- scripts/update-configs.sh
+++ scripts/update-configs.sh
@@ -1,17 +1,19 @@
-cd ~/Drive/
+cd ~/Documents/config/
 
-grive
+cp ~/Documents/config/config_files/config ~/.config/i3/
+echo copied i3 config
 
-cp ~/Drive/config_files/config ~/.config/i3/
+cp ~/Documents/config/config_files/rc.conf ~/.config/ranger/
+echo copied rc.conf
 
-cp ~/Drive/config_files/rc.conf ~/.config/ranger/
+cp ~/Documents/config/config_files/.vimrc ~/
+echo copied .vimrc
 
-cp ~/Drive/config_files/.vimrc ~/
+cp ~/Documents/config/config_files/.i3blocks.conf ~/
+echo copied i3blocks.conf
 
-cp ~/Drive/config_files/.Xresources ~/
+cp ~/Documents/config/config_files/.profile ~/
+echo copied .profile
 
-cp ~/Drive/config_files/.i3blocks.conf ~/
-
-cp ~/Drive/config_files/.profile ~/
-
-cp ~/Drive/.newsboat ~/.newsboat
+cp ~/Documents/config/config_files/.bashrc ~/
+echo copied .bashrc
